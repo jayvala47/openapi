b59df098b3a18316ed95f9a284c578994dd554ab
root <root@ip-172-31-37-114.ec2.internal>
2020-04-30 13:54:12 +0000

    Set an agrument method in payload shell script

--- payload.sh
+++ payload.sh
@@ -2,6 +2,6 @@
 v=$1
 curl -X POST http://ec2-204-236-241-168.compute-1.amazonaws.com:8080/job/swagger_update/build \
   --user admin:11800846c23b5eba4c0874c4bb84deaad7 \
-  --data-urlencode json='{"parameter": [{"name":"schema", "value":"testV3"}]}'
+  --data-urlencode json='{"parameter": [{"name":"schema", "value":"'"$v"'"}]}'
 
 echo schema ref version is $1
